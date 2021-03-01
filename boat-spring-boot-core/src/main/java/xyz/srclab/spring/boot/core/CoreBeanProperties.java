e1b53b7ceadb96ae7d5aec6ad256754678ea28a4
SrcLab <fredsuvn@163.com>
2021-03-02 02:19:12 +0800

    add kapt

---
+++ boat-spring-boot-core/src/main/java/xyz/srclab/spring/boot/core/CoreBeanProperties.java
@@ -0,0 +1,14 @@
+package xyz.srclab.spring.boot.core;
+
+public class CoreBeanProperties {
+
+    private String coreProperty;
+
+    public String getCoreProperty() {
+        return coreProperty;
+    }
+
+    public void setCoreProperty(String coreProperty) {
+        this.coreProperty = coreProperty;
+    }
+}
--- boat-spring-boot-core/src/main/kotlin/xyz/srclab/spring/boot/core/CoreBeanProperties.kt
+++ boat-spring-boot-core/src/main/kotlin/xyz/srclab/spring/boot/core/CoreBeanProperties.kt
@@ -1,12 +1,11 @@
-package xyz.srclab.spring.boot.core
-
-import org.springframework.boot.context.properties.ConfigurationProperties
-
-/**
- * @author sunqian
- */
-@ConfigurationProperties(prefix = "boat.core")
-open class CoreBeanProperties {
-
-    var coreProperty: String? = null
-}
+//package xyz.srclab.spring.boot.core
+//
+//import org.springframework.boot.context.properties.ConfigurationProperties
+//
+///**
+// * @author sunqian
+// */
+//open class CoreBeanProperties {
+//
+//    var coreProperty: String? = null
+//}
