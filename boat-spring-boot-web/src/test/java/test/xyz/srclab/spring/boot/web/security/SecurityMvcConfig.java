af85606e6ccf7da52a4e0dad6650a50ab062190e
fredsuvn <fredsuvn@163.com>
2021-06-09 17:47:43 +0800

    add security

---
+++ boat-spring-boot-web/src/test/java/test/xyz/srclab/spring/boot/web/security/SecurityMvcConfig.java
@@ -0,0 +1,20 @@
+package test.xyz.srclab.spring.boot.web.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * @author sunqian
+ */
+@Configuration
+public class SecurityMvcConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/home").setViewName("home");
+        registry.addViewController("/").setViewName("home");
+        registry.addViewController("/hello").setViewName("hello");
+        registry.addViewController("/login").setViewName("login");
+    }
+}
